fcdbc5312ac7dffee9316df33bcb4c170a662b2f
Vitaliy Stepanenko <mail@vitaliy.in>
2015-04-22 19:47:30 +0300

    Tests added

--- tests/Components/ContainerTest.php
+++ tests/Components/ContainerTest.php
@@ -3,6 +3,9 @@ namespace Nayjest\ViewComponents\Test\Components;
 
 use Nayjest\ViewComponents\Components\Container;
 use Nayjest\ViewComponents\Components\Text;
+use Nayjest\ViewComponents\Test\Mock\ChildClass;
+use Nayjest\ViewComponents\Test\Mock\HierarchyItem;
+use Nayjest\ViewComponents\Test\Mock\ParentClass;
 use PHPUnit_Framework_TestCase;
 
 class ContainerTest extends PHPUnit_Framework_TestCase
@@ -24,5 +27,29 @@ class ContainerTest extends PHPUnit_Framework_TestCase
         $c->components()->add($c2);
         $c2->setComponents([$t, $t]);
         self::assertEquals('[{inner}]', $c->render());
+    }
+
+    public function testNotRenderableComponents()
+    {
+        // Renderable container can store not renderable items.
+        $container = new Container([
+            new ChildClass,
+            new Text('test')
+        ]);
+        self::assertEquals(
+            'test',
+            $container->render()
+        );
+
+        // Renderable store not renderable that store renderable
+        $container->setComponents([
+            new Text('ok'),
+            (new HierarchyItem)
+                ->setComponents([new Text('Must not be rendered')])
+        ]);
+        self::assertEquals(
+            'ok',
+            $container->render()
+        );
     }
 }
