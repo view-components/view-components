450b67d500591cb965342cbf1720363d33c0b3fb
Vitaliy Stepanenko <mail@vitaliy.in>
2015-05-08 18:26:13 +0300

    ViewComponentAggregate implemented

---
+++ src/BaseComponents/ViewComponentAggregateTrait.php
@@ -0,0 +1,40 @@
+<?php
+namespace Nayjest\ViewComponents\BaseComponents;
+
+use Nayjest\ViewComponents\Rendering\ViewAggregateTrait;
+
+/**
+ * Class ViewComponentAggregateTrait
+ *
+ * Extended ViewAggregate that implements ContainerInterface
+ *
+ */
+trait ViewComponentAggregateTrait
+{
+    use ContainerTrait {
+        ContainerTrait::components as protected internalComponents;
+        ContainerTrait::setComponents as private internalSetComponents;
+    }
+    use ViewAggregateTrait {
+        ViewAggregateTrait::setView as protected setViewInternal;
+    };
+
+    public function render()
+    {
+        return $this->renderComponents(null);
+    }
+
+    public function setView(ComponentInterface $view)
+    {
+        if ($this->view !== null) {
+            $this->internalComponents()->remove($this->view);
+        }
+        $this->setViewInternal($view);
+        $this->internalComponents()->add($this->view);
+    }
+
+    public function components()
+    {
+        return $this->internalComponents()->readonly();
+    }
+}
