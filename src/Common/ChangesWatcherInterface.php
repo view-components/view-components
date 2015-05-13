16d6fffad7d4741be58bb812f035530413123002
Vitaliy Stepanenko <mail@vitaliy.in>
2015-05-13 17:52:02 +0300

    phpDoc

--- src/Common/ChangesWatcherInterface.php
+++ src/Common/ChangesWatcherInterface.php
@@ -2,7 +2,20 @@
 
 namespace Nayjest\ViewComponents\Common;
 
+/**
+ * Interface ChangesWatcherInterface
+ *
+ * The interface describes objects that can track it's state changes.
+ *
+ * @package Nayjest\ViewComponents\Common
+ */
 interface ChangesWatcherInterface
 {
+    /**
+     * Returns true if object was changed since last call.
+     * When called first time, returns true.
+     *
+     * @return bool
+     */
     public function isChanged();
 }
