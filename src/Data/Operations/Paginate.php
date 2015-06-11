58b8abf1a5012b565b634b4cb5ce29dee6715320
Vitaliy Stepanenko <mail@vitaliy.in>
2015-06-11 21:36:18 +0300

    Paginate operation added

---
+++ src/Data/Operations/Paginate.php
@@ -0,0 +1,52 @@
+<?php
+namespace Nayjest\ViewComponents\Data\Operations;
+
+
+class Paginate implements OperationInterface
+{
+    protected $pageNumber;
+
+    protected $pageSize;
+
+    public function __construct($pageNumber = 1, $pageSize = 50)
+    {
+        $this->pageNumber = $pageNumber;
+        $this->pageSize = $pageSize;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPageNumber()
+    {
+        return $this->pageNumber;
+    }
+
+    /**
+     * @param $pageNumber
+     * @return $this
+     */
+    public function setPageNumber($pageNumber)
+    {
+        $this->pageNumber = $pageNumber;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPageSize()
+    {
+        return $this->pageSize;
+    }
+
+    /**
+     * @param $pageSize
+     * @return $this
+     */
+    public function setPageSize($pageSize)
+    {
+        $this->pageSize = $pageSize;
+        return $this;
+    }
+}
