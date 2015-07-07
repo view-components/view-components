1466dbcd88d929300436328fd193b7abaacdfb6f
Vitaliy Stepanenko <mail@vitaliy.in>
2015-07-07 19:27:12 +0300

    Unused "use" statements removed

--- tests/webapp/AbstractController.php
+++ tests/webapp/AbstractController.php
@@ -1,25 +1,6 @@
 <?php
 namespace Presentation\Framework\Demo;
 
-use Presentation\Framework\Common\InputOption;
-use Presentation\Framework\Common\ListManager;
-use Presentation\Framework\Components\Container;
-use Presentation\Framework\Components\Controls\FilterControl;
-use Presentation\Framework\Components\ManagedList;
-use Presentation\Framework\Components\Debug\SymfonyVarDump;
-use Presentation\Framework\Components\Html\Tag;
-use Presentation\Framework\Data\ArrayDataProvider;
-use Presentation\Framework\Data\DbTableDataProvider;
-use Presentation\Framework\Data\Operations\FilterOperation;
-use Presentation\Framework\Data\Operations\SortOperation;
-use Presentation\Framework\HtmlBuilder;
-use Presentation\Framework\Components\Repeater;
-use Presentation\Framework\Components\Text;
-use Presentation\Framework\Demo\Components\PersonView;
-use Presentation\Framework\Resources\AliasRegistry;
-use Presentation\Framework\Resources\IncludedResourcesRegistry;
-use Presentation\Framework\Resources\Resources;
-use Presentation\Framework\Styling\Bootstrap\BootstrapStyling;
 use ReflectionClass;
 use ReflectionMethod;
 
