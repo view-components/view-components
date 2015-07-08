0a9a16369bb6f82f9b2140d7c648b5dc7e3b49d7
Vitaliy Stepanenko <mail@vitaliy.in>
2015-07-08 14:20:00 +0300

    phpDoc

--- src/BaseComponents/Controls/ControlInterface.php
+++ src/BaseComponents/Controls/ControlInterface.php
@@ -6,6 +6,8 @@ use Presentation\Framework\Data\Operations\OperationInterface;
 interface ControlInterface
 {
     /**
+     * Creates operation.
+     *
      * @return OperationInterface
      */
     public function getOperation();
