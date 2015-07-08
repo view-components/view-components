52c39185ecc377dccb3d471cdb8202f24ac15b07
Vitaliy Stepanenko <mail@vitaliy.in>
2015-07-08 20:08:24 +0300

    phpdoc

--- src/Common/InputOptionFactory.php
+++ src/Common/InputOptionFactory.php
@@ -9,19 +9,37 @@ class InputOptionFactory
      */
     private $input;
 
+    /**
+     * Constructor.
+     *
+     * @param array $input $_GET, $_POST, etc. can be used as input
+     */
     public function __construct(array $input)
     {
         $this->input = $input;
     }
 
+    /**
+     * Creates input option.
+     *
+     * @param string $name
+     * @param null $default optional default value
+     * @return InputOption
+     */
     public final function make($name, $default = null)
     {
         return new InputOption($name, $this->input, $default);
     }
 
+    /**
+     * Shortcut for InputOptionFactory::make.
+     *
+     * @param string $name
+     * @param null $default optional default value
+     * @return InputOption
+     */
     public function __invoke($name, $default = null)
     {
         return $this->make($name, $default);
     }
-
 }
