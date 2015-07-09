abbb861edffd21ea244366da4e7ce371ed58b79f
Vitaliy Stepanenko <mail@vitaliy.in>
2015-07-09 11:36:18 +0300

    phpDoc, exception message changed

--- src/Collection/Collection.php
+++ src/Collection/Collection.php
@@ -8,6 +8,13 @@ class Collection implements CollectionReadInterface
 {
     protected $items = [];
 
+    /**
+     * Adds item to collection.
+     *
+     * @param $item
+     * @param bool $prepend false by default
+     * @return $this
+     */
     public function add($item, $prepend = false)
     {
         if ($prepend) {
@@ -18,38 +25,69 @@ class Collection implements CollectionReadInterface
         return $this;
     }
 
+    /**
+     * Removes item from collection.
+     *
+     * @param $item
+     * @return $this
+     */
     public function remove($item)
     {
         $key = array_search($item, $this->items, true);
         if ($key === false) {
             throw new RuntimeException(
-                'Removing nonexistent item from collection.'
+                'Trying to remove nonexistent item from collection.'
             );
         }
         unset($this->items[$key]);
         return $this;
     }
 
+    /**
+     * Returns collection items in array.
+     *
+     * @return array
+     */
     public function toArray()
     {
         return $this->items;
     }
 
+    /**
+     * Returns true if collection is empty.
+     *
+     * @return bool
+     */
     public function isEmpty()
     {
         return count($this->items) === 0;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function count()
     {
         return count($this->items);
     }
 
+    /**
+     * Checks that collections contains target item.
+     *
+     * @param $item
+     * @return bool
+     */
     public function has($item)
     {
         return in_array($item, $this->items, true);
     }
 
+    /**
+     * Sets collection items.
+     *
+     * @param array $items
+     * @return $this
+     */
     public function set(array $items)
     {
         $this->clear();
@@ -59,12 +97,20 @@ class Collection implements CollectionReadInterface
         return $this;
     }
 
+    /**
+     * Removes all items from collection.
+     *
+     * @return $this
+     */
     public function clear()
     {
         $this->items = [];
         return $this;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getIterator()
     {
         return new ArrayIterator($this->items);
