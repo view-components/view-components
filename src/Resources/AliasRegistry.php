60f5d56b98a730c6c66ead571d036350975525d4
Vitaliy Stepanenko <mail@vitaliy.in>
2015-09-01 17:55:55 +0300

    phpDoc

--- src/Resources/AliasRegistry.php
+++ src/Resources/AliasRegistry.php
@@ -1,6 +1,13 @@
 <?php
 namespace Presentation\Framework\Resources;
 
+/**
+ * Class AliasRegistry.
+ *
+ * The class implements registry of aliases.
+ *
+ * @package Presentation\Framework\Resources
+ */
 class AliasRegistry
 {
     protected $aliases;
@@ -15,6 +22,14 @@ class AliasRegistry
         $this->aliases = $aliases;
     }
 
+    /**
+     * Creates/updates alias.
+     *
+     * @param string $name
+     * @param string $url
+     * @param bool $overwrite (false by default)
+     * @return bool true if alias was created/updated.
+     */
     public function set($name, $url, $overwrite = false)
     {
         if (!$overwrite && array_key_exists($name, $this->aliases)) {
@@ -24,6 +39,13 @@ class AliasRegistry
         return true;
     }
 
+    /**
+     * Returns aliased value or default value if requested alias does not exists.
+     *
+     * @param string $name
+     * @param null $default
+     * @return null
+     */
     public function get($name, $default = null)
     {
         if (array_key_exists($name, $this->aliases)) {
@@ -33,6 +55,12 @@ class AliasRegistry
         }
     }
 
+    /**
+     * Returns true if alias exists.
+     *
+     * @param string $name
+     * @return bool
+     */
     public function has($name)
     {
         return array_key_exists($name, $this->aliases);
