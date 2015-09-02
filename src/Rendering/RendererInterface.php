fffa67fccd66e260d066bef9f44afb64fc2ac68b
Vitaliy Stepanenko <mail@vitaliy.in>
2015-09-02 19:28:09 +0300

    working on rendering system

---
+++ src/Rendering/RendererInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Presentation\Framework\Rendering;
+
+
+interface RendererInterface
+{
+    /**
+     * @param string $template
+     * @param array $viewData
+     * @return string
+     */
+    public function render($template, array $viewData = null);
+}
