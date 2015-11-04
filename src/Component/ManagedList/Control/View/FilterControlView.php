03d7c7e5e04c52d3f478bdc0f2e233e832c00ff7
Vitaliy Stepanenko <mail@vitaliy.in>
2015-11-04 18:17:23 +0200

    FilterControlView improvements

--- src/Component/ManagedList/Control/View/FilterControlView.php
+++ src/Component/ManagedList/Control/View/FilterControlView.php
@@ -13,6 +13,19 @@ use Presentation\Framework\Component\Text;
 class FilterControlView extends Tag
 {
     /**
+     * @var string
+     */
+    private $name;
+    /**
+     * @var null|string
+     */
+    private $value;
+    /**
+     * @var null|string
+     */
+    private $label;
+
+    /**
      * Constructor.
      *
      * @param string $name
@@ -21,27 +34,80 @@ class FilterControlView extends Tag
      */
     public function __construct($name, $value = null, $label = null)
     {
-        $id = $name;
+        $this->name = $name;
+        $this->value = $value;
+        $this->label = $label;
         parent::__construct(
             'span',
             [
                 'data-role' => 'control-container'
-            ],
-            [
+            ]
+        );
+        $this->onRender(function() {
+            $id = $this->name;
+            $this->addChildren([
                 new Tag('label', [
                     'for' => $id
                 ], [
-                    new Text($label)
+                    new Text($this->label)
                 ]),
                 new Text('&nbsp;'),
                 new Tag('input', [
-                    'value' => $value,
+                    'value' => $this->value,
                     'type' => 'text',
-                    'name' => $name,
+                    'name' => $this->name,
                     'id' => $id
                 ]),
                 new Text('&nbsp;')
-            ]
-        );
+            ]);
+        });
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param null|string $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * @param null|string $label
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
     }
 }
