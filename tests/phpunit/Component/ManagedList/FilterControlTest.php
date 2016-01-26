b1b0a076876d934346c8a27cd18da3190abac3cc
v.stepanenko <mail@vitaliy.in>
2016-01-26 15:36:29 +0200

    Tests: FilterControlTest added

---
+++ tests/phpunit/Component/ManagedList/FilterControlTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Presentation\Framework\Test\Component\ManagedList;
+
+use PHPUnit_Framework_TestCase;
+use Presentation\Framework\Component\ManagedList\Control\FilterControl;
+use Presentation\Framework\Data\Operation\FilterOperation;
+use Presentation\Framework\Input\InputOption;
+
+class FilterControlTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testDefaultView()
+    {
+        $filter = new FilterControl(
+            'some_field',
+            FilterOperation::OPERATOR_EQ,
+            new InputOption('fld', [], 5)
+        );
+        $filter->useDefaultView();
+        self::assertTrue($filter->children()->count() > 0);
+    }
+}
