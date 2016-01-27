6006a3e4d8a7c4e4675929c189a3045613e8ceb7
v.stepanenko <mail@vitaliy.in>
2016-01-27 18:33:38 +0200

    Refactored adding compound parts. Fix: now setRootInternal() will be always called for compoundParts

--- src/Common/CompoundPartCollection.php
+++ src/Common/CompoundPartCollection.php
@@ -21,32 +21,46 @@ class CompoundPartCollection extends NodeCollection
     /**
      * @param $component
      * @param bool|false $prepend
-     * @return bool
      */
-    protected function tryAddCompoundPart($component, $prepend = false)
+    protected function addCompoundPart(CompoundPartInterface $component, $prepend = false)
     {
-        /** @var CompoundComponent $root */
-        $root = $this->parentNode;
-        if (!$component instanceof CompoundPartInterface) {
-            return false;
-        }
-        $parentName = $component->resolveParentName($this->getCompoundRoot());
-        if ($parentName === null) {
-            return false;
-        }
-        if (!$root->hasComponent($parentName)) {
+        $this->placeInTree($component, $prepend);
+        $component->setRootInternal($this->getCompoundRoot());
+    }
+
+    /**
+     * @param CompoundPartInterface $component
+     * @param bool $prepend
+     */
+    private function placeInTree(CompoundPartInterface $component, $prepend = false)
+    {
+        $root = $this->getCompoundRoot();
+        $parentName = $component->resolveParentName($root);
+        if ($parentName !== null && !$root->hasComponent($parentName)) {
             throw new NodeNotFoundException;
         }
-        $componentName = $component->getComponentName();
-        if (!$componentName) {
-            $componentName = $this->getCompoundRoot()->getComponentName() . '_component_' . rand();
-            $component->setComponentName($componentName);
-        }
-        $component->setRootInternal($this->getCompoundRoot());
+        $this->provideNameIfEmpty($component);
         $prepend
-            ? $root->prependComponent($parentName, $componentName, $component)
-            : $root->appendComponent($parentName, $componentName, $component);
-        return true;
+            ? $root->prependComponent($parentName, $component->getComponentName(), $component)
+            : $root->appendComponent($parentName, $component->getComponentName(), $component);
+    }
+
+    /**
+     * Adding to tree requires nodes naming.
+     * Node names are stored by tree internally, so there is no need of storing name inside node.
+     * However we use component_name as tree node name for convenience
+     * since it can't be returned by $collection->add.
+     * Therefore we need to provide component_name if it's empty.
+     *
+     * @param CompoundPartInterface $component
+     */
+    private function provideNameIfEmpty(CompoundPartInterface $component)
+    {
+        if (!$component->getComponentName()) {
+            $component->setComponentName(
+                $this->getCompoundRoot()->getComponentName() . '_component_' . rand()
+            );
+        }
     }
 
     /**
@@ -56,7 +70,9 @@ class CompoundPartCollection extends NodeCollection
      */
     public function add($component, $prepend = false)
     {
-        if (!$this->tryAddCompoundPart($component, $prepend)) {
+        if ($component instanceof CompoundPartInterface) {
+            $this->addCompoundPart($component, $prepend);
+        } else {
             parent::add($component, $prepend);
         }
         return $this;
