c4eed3097ef8d43fef2d9256916e5ccca973b538
v.stepanenko <mail@vitaliy.in>
2016-02-26 20:19:14 +0200

    Select component added

---
+++ src/Component/Html/Select.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ViewComponents\ViewComponents\Component\Html;
+
+use Nayjest\Tree\ChildNodeInterface;
+use ViewComponents\ViewComponents\Base\Html\TagInterface;
+use ViewComponents\ViewComponents\Component\DataView;
+
+class Select extends Tag
+{
+    /**
+     * @param array|null $attributes
+     * @param array $options components or 'value' => 'label' array
+     * @param string $selectedValue
+     */
+    public function __construct(
+        array $attributes = [],
+        array $options = [],
+        $selectedValue = null
+    )
+    {
+        parent::__construct(
+            'select',
+            $attributes,
+            self::makeOptionComponents($options)
+        );
+        if ($selectedValue !== null) {
+            $this->setOptionSelected($selectedValue);
+        }
+    }
+
+    private function setOptionSelected($selectedValue)
+    {
+        /** @var TagInterface $option */
+        foreach($this->children() as $option)
+        {
+            if (!$option instanceof TagInterface) {
+                continue;
+            }
+            if ((string)$option->getAttribute('value') === (string)$selectedValue) {
+                // @todo setAttribute not in tag interface
+                $option->setAttribute('selected', 'selected');
+            }
+        }
+    }
+
+    private static function makeOptionComponents($options)
+    {
+        $components = [];
+        foreach($options as $value => $label)
+        {
+            if ($label instanceof ChildNodeInterface) {
+                $components[] = $label;
+            } else {
+                $components[] = new Tag(
+                    'option',
+                    ['value' => $value],
+                    [new DataView($label)]
+                );
+            }
+        }
+        return $components;
+    }
+}
