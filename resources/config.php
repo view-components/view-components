0612f0fa1d7caa4208476152df5d7b98e0fe6973
v.stepanenko <mail@vitaliy.in>
2016-03-03 19:22:11 +0200

    Google Material Design aliases added to config, see http://www.getmdl.io/started/

--- resources/config.php
+++ resources/config.php
@@ -8,9 +8,12 @@ return [
     'js_aliases' => [
         'jquery' =>'//code.jquery.com/jquery-2.1.4.min.js',
         'bootstrap' => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css',
+        'google_material' => 'https://code.getmdl.io/1.1.1/material.min.js',
     ],
     /** Will be passed to AliasRegistry constructor. @see AliasRegistry::__construct() **/
     'css_aliases' => [
         'bootstrap' => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css',
+        'google_material' => 'https://code.getmdl.io/1.1.1/material.indigo-pink.min.css',
+        'google_material_icons' => 'https://fonts.googleapis.com/icon?family=Material+Icons'
     ]
 ];
