b3c301079435a0073a2669bd9c240d2c5314f848
v.stepanenko <mail@vitaliy.in>
2016-03-09 17:38:08 +0200

    twitter bootstrap js alias fix & update to 3.3.6

--- resources/config.php
+++ resources/config.php
@@ -7,12 +7,12 @@ return [
     /** Will be passed to AliasRegistry constructor. @see AliasRegistry::__construct() **/
     'js_aliases' => [
         'jquery' =>'//code.jquery.com/jquery-2.2.1.min.js',
-        'bootstrap' => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css',
+        'bootstrap' => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js',
         'google_material' => 'https://code.getmdl.io/1.1.1/material.min.js',
     ],
     /** Will be passed to AliasRegistry constructor. @see AliasRegistry::__construct() **/
     'css_aliases' => [
-        'bootstrap' => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css',
+        'bootstrap' => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
         'google_material' => 'https://code.getmdl.io/1.1.1/material.indigo-pink.min.css',
         'google_material_icons' => 'https://fonts.googleapis.com/icon?family=Material+Icons'
     ]
