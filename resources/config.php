865ba1d029ff2765efaf71f4dd8d768c9b754074
v.stepanenko <mail@vitaliy.in>
2016-03-09 17:18:38 +0200

    jquery update to v2.2.1

--- resources/config.php
+++ resources/config.php
@@ -6,7 +6,7 @@
 return [
     /** Will be passed to AliasRegistry constructor. @see AliasRegistry::__construct() **/
     'js_aliases' => [
-        'jquery' =>'//code.jquery.com/jquery-2.1.4.min.js',
+        'jquery' =>'//code.jquery.com/jquery-2.2.1.min.js',
         'bootstrap' => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css',
         'google_material' => 'https://code.getmdl.io/1.1.1/material.min.js',
     ],
