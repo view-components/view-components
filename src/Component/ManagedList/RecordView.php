4de6490193e05adc6a37205008536860bd72f30b
v.stepanenko <mail@vitaliy.in>
2016-03-15 15:37:17 +0200

    Code style fix

--- src/Component/ManagedList/RecordView.php
+++ src/Component/ManagedList/RecordView.php
@@ -3,13 +3,19 @@
 namespace ViewComponents\ViewComponents\Component\ManagedList;
 
 use ViewComponents\ViewComponents\Base\ViewComponentInterface;
+use ViewComponents\ViewComponents\Component\ManagedList;
 use ViewComponents\ViewComponents\Component\Part;
 
 class RecordView extends Part
 {
     const ID = 'record_view';
+
+    /**
+     * RecordView constructor.
+     * @param ViewComponentInterface $view
+     */
     public function __construct(ViewComponentInterface $view)
     {
-        parent::__construct($view, static::ID, 'collection_view');
+        parent::__construct($view, static::ID, ManagedList::COLLECTION_VIEW_ID);
     }
 }
