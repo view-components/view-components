593402468d871c8c3b794f45a1e03f374d913658
v.stepanenko <mail@vitaliy.in>
2016-04-07 17:17:40 +0300

    Twitter Bootstrap customization: bootstrap-datepicker added

--- resources/config/config.php
+++ resources/config/config.php
@@ -10,11 +10,13 @@ return [
         'bootstrap' => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js',
         'foundation' => 'https://cdn.jsdelivr.net/foundation/6.2.0/foundation.min.js',
         'semantic-ui' => 'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.8/semantic.min.js',
+        'bootstrap-datepicker' => 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/js/bootstrap-datepicker.min.js',
     ],
     /** Will be passed to AliasRegistry constructor. @see AliasRegistry::__construct() **/
     'css_aliases' => [
         'bootstrap' => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
         'foundation' => 'https://cdn.jsdelivr.net/foundation/6.2.0/foundation.min.css',
         'semantic-ui' => 'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.8/semantic.min.css',
+        'bootstrap-datepicker' => 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker.min.css',
     ]
 ];
--- resources/config/styling/twitter_bootstrap.php
+++ resources/config/styling/twitter_bootstrap.php
@@ -16,22 +16,37 @@ $helper = new ViewHelper([
 
 return [
     'root' => [
-        'js:jquery',
-        'js:bootstrap',
+        ['js' => [['jquery', 'bootstrap'], true]],
         'css:bootstrap'
     ],
     'compound_part#form' => 'add_class:form-inline',
     'tag&property:type,submit' => [
         'add_class:' . $helper->getButtonClasses('btn-success'),
-        $useIcons ? 'prepend_text:<i class="glyphicon glyphicon-refresh"></i>&nbsp;': []
+        $useIcons ? 'prepend_text:<i class="glyphicon glyphicon-refresh"></i>&nbsp;' : []
     ],
-    'tag:table' => 'add_class: '. $helper->getTableClasses(),
-    'tag:button&property:type,button' =>  'add_class: '. $helper->getButtonClasses(),
+    'tag:table' => 'add_class: ' . $helper->getTableClasses(),
+    'tag:button&property:type,button' => 'add_class: ' . $helper->getButtonClasses(),
     'tag:button&property:type,reset' =>
         [
             ['add_class' => $helper->getButtonClasses('btn-warning')],
             $useIcons ? ['prepend_text' => '<i class="glyphicon glyphicon-erase"></i>&nbsp;'] : []
         ],
+    'template:input&dataItem:inputType,date' => [
+        'js:bootstrap-datepicker',
+        'css:bootstrap-datepicker',
+        [
+            'merge-data' => [
+                [
+                    'inputClass' => 'datepicker',
+                    'inputType' => 'text',
+                    'inputAttributes' => [
+                        'data-provide' => 'datepicker',
+                        'data-date-format' => 'yyyy-mm-dd'
+                    ]
+                ]
+            ]
+        ],
+    ],
     'template' => [
         ['override_template' => ['themes/twitter_bootstrap', compact('helper')]]
     ]
